79b5a7bc55970d160da209df255a0fa2a289bc6c
Ferdinand Ramdhani <ferdinram13@gmail.com>
2023-11-26 19:13:34 +0700

    addBookHandler finsih

--- src/handler.js
+++ src/handler.js
@@ -1,7 +1,8 @@
-const { request } = require('http');
+const { nanoid } = require('nanoid');
 const books = require('./books');
 
-const addNoteHandler = (require, h) => {
+// eslint-disable-next-line consistent-return
+const addBookHandler = (request, h) => {
    const {
       name,
       year,
@@ -10,6 +11,70 @@ const addNoteHandler = (require, h) => {
       publisher,
       pageCount,
       readPage,
-      reading
+      reading,
    } = request.payload;
-   
+
+   if (!name) {
+      const response = h.response({
+         status: 'fail',
+         message: 'Gagal menambahkan buku. Mohon isi nama buku',
+      });
+      response.code(400);
+      return response;
+   }
+
+   if (readPage > pageCount) {
+      const response = h.response({
+         status: 'fail',
+         message: 'Gagal menambahkan buku. readPage tidak boleh lebih besar dari pageCount',
+      });
+      response.code(400);
+      return response;
+   };
+
+   const id = nanoid(16);
+   const insertedAt = new Date().toISOString();
+   const updatedAt = insertedAt;
+   const finished = readPage === pageCount;
+
+   const newBook = {
+      id,
+      name,
+      year,
+      author,
+      summary,
+      publisher,
+      pageCount,
+      readPage,
+      finished,
+      reading,
+      insertedAt,
+      updatedAt,
+   };
+
+   books.push(newBook);
+   const isSuccess = books.filter((note) => note.id === id).length > 0;
+
+   if (isSuccess) {
+      const response = h.response({
+         status: 'success',
+         message: 'Buku berhasil ditambahkan',
+         data: {
+            bookId: id,
+         },
+      });
+      response.code(201);
+      return response;
+   }
+
+   const response = h.response({
+      status: 'fail',
+      message: 'Buku gagal ditambahkan',
+   });
+   response.code(500);
+   return response;
+};
+
+module.exports = {
+   addBookHandler,
+};
--- src/routes.js
+++ src/routes.js
@@ -1,3 +1,2 @@
 const routes = [];
-
 module.exports = routes;
